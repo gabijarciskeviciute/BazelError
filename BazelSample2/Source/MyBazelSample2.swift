79e45002e48d01e1cdbce493950bc7857d1d6b38
gabijarciskeviciute <gabijarciskeviciute@gmail.com>
2022-09-15 10:01:49 -0400

    Adding swift files and srcs block

---
+++ BazelSample2/Source/MyBazelSample2.swift
@@ -0,0 +1,14 @@
+//
+//  MyBazelSample2.swift
+//  BazelSample2
+//
+//  Created by Gabija Arciskeviciute on 9/15/22.
+//
+
+import Foundation
+
+class MyBazelSampleError {
+    func helloBazelSampleError() {
+        print("Hello, BazelSample2!")
+    }
+}
---
+++ BazelSampleError/Source/MyBazelSampleError.swift
@@ -0,0 +1,14 @@
+//
+//  MyBazelSampleError.swift
+//  BazelSampleError
+//
+//  Created by Gabija Arciskeviciute on 9/15/22.
+//
+
+import Foundation
+
+class MyBazelSampleError {
+    func helloBazelSampleError() {
+        print("Hello, BazelSampleError!")
+    }
+}
